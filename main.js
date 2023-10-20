b2388b8330e5947f138c05981be6f0a1d881353f
Chris Nelson <chrisnelson7791@gmail.com>
2023-10-20 16:14:12 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function hero(bullets, dragons){
+  return bullets >= dragons * 2 ? true : false
+}
+  
+  
+  // p: bullets, dragons    // numbers
+  // r: boolean
+  // e.g. hero(7, 3)
+  // results in true
+
+  // return ternary expression evaluating true if bullets is greater than or equal to dragons * 2
